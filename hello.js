930c364776fffcd04815ba87197aa056e129d201
Demilade30 <>
2021-05-20 04:38:36 +0000

    latest

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints 
 	// when you hit the replit run button
-	console.log('Hello world!');
+	console.log ('Hello world!');
 }
  
 module.exports = hello;
